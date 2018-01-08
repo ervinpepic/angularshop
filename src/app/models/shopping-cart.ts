26798e9a509355eb424cff48e02c206df5883204
Ervin <ervin@Ervins-iMac.local>
2018-01-08 16:15:31 +0100

    Added ability to update and remove products diretcly from shopping cart instead of home page early

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -4,18 +4,18 @@ import { Product } from "./product";
 export class ShoppingCart {
     items: ShoppingCartItem[] = [];
     constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+        this.itemsMap = itemsMap || {};
 
-        for (let productId in itemsMap){
+        for (let productId in itemsMap) {
             let item = itemsMap[productId];
-            this.items.push(new ShoppingCartItem(item.product, item.quantity));
+            this.items.push(new ShoppingCartItem({...item, $key: productId }));
         }
-        
     }
 
     getQuantity(product: Product) {
         let item = this.itemsMap[product.$key];
         return item ? item.quantity : 0;
-      }
+    }
 
     get totalPrice() {
         let sum = 0;
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -10,16 +10,35 @@ export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
+
+  //public methods
+  async getCart(): Promise<Observable<ShoppingCart>> {
+    let cartId = await this.getOrCreateCartId();
+    return this.db.object('/shopping-carts/' + cartId)
+      .map(x => new ShoppingCart(x.items));
+  }
+
+  async addToCart(product: Product) {
+    this.updateItem(product, 1);
+
+  }
+
+  async removeFromCart(product: Product) {
+    this.updateItem(product, -1);
+  }
+
+  async clearCart() {
+    let cartId = await this.getOrCreateCartId();
+    this.db.object('/shopping-carts/' + cartId + '/items').remove();
+  }
+
+
+
+  //private methods
   private create() {
     return this.db.list('/shopping-carts').push({
       dateCreated: new Date().getTime()
     });
-  }
-
-  async getCart(): Promise<Observable<ShoppingCart>> {
-    let cartId = await this.getOrCreateCartId();
-    return this.db.object('/shopping-carts/' + cartId)
-    .map(x => new ShoppingCart(x.items));
   }
 
   private getItem(cartId: string, productId: string) {
@@ -36,23 +55,19 @@ export class ShoppingCartService {
 
   }
 
-  private async updateItemQuantity(product: Product, change: number) {
+  private async updateItem(product: Product, change: number) {
     let cartId = await this.getOrCreateCartId();
     let item$ = this.getItem(cartId, product.$key);
     item$.take(1).subscribe(item => {
-      item$.update({ product: product, quantity: (item.quantity || 0) + change });
+      let quantity = (item.quantity || 0) + change;
+      if (quantity === 0) item$.remove();
+      else item$.update({
+        title: product.title,
+        imageUrl: product.imageUrl,
+        price: product.price,
+        quantity: quantity
+      });
     });
   }
-
-  async addToCart(product: Product) {
-    this.updateItemQuantity(product, 1);
-
-  }
-
-  async removeFromCart(product: Product) {
-    this.updateItemQuantity(product, -1);
-  }
-
-
 
 }
