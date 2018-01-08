7525eda63ef538bf30e2ba5b9efe252f2601aac8
Ervin <ervin@Ervins-iMac.local>
2018-01-08 16:50:14 +0100

    Bug fixes in shopping cart when items go to negative

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../product.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
-import 'rxjs/add/operator/switchMap';
 import { Subscription } from 'rxjs/Subscription';
 import { ShoppingCartService } from '../shopping-cart.service';
+import { Observable } from 'rxjs/Observable';
+import { ShoppingCart } from '../models/shopping-cart';
+
+import 'rxjs/add/operator/switchMap';
 
 
 @Component({
@@ -12,43 +15,48 @@ import { ShoppingCartService } from '../shopping-cart.service';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit, OnDestroy {
+export class ProductsComponent implements OnInit {
   products: Product[] = [];
   filteredProducts: Product[] = [];
   category: string;
-  cart: any;
-  subsrciption: Subscription
+  cart$: Observable<ShoppingCart>;
+
 
   constructor(
-    route: ActivatedRoute,
-    productService: ProductService,
+    private route: ActivatedRoute,
+    private productService: ProductService,
     private shoppingCartService: ShoppingCartService
-    ) {
-      
+  ) {
 
-    productService
+
+
+  }
+  async ngOnInit() {
+    this.cart$ = await this.shoppingCartService.getCart();
+    this.populateProducts();
+
+  }
+
+  private populateProducts() {
+
+    this.productService
       .getAllproducts()
       .switchMap(products => {
         this.products = products;
-        return route.queryParamMap;
+        return this.route.queryParamMap;
       })
 
       .subscribe(params => {
         this.category = params.get('category');
-
-        this.filteredProducts = (this.category) ?
-          this.products.filter(p => p.category === this.category) :
-          this.products;
+        this.applyFilter();
       });
 
   }
-async ngOnInit() {
-  this.subsrciption = (await this.shoppingCartService.getCart())
-    .subscribe(cart => this.cart = cart);
-}
 
-ngOnDestroy() {
-  this.subsrciption.unsubscribe();
-}
+  private applyFilter() {
+    this.filteredProducts = (this.category) ?
+      this.products.filter(p => p.category === this.category) :
+      this.products;
+  }
 
 }
