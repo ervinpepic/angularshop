061ddd1f3beac89b928d5fb65f9531390f496bd2
Ervin <ervin@Ervins-iMac.local>
2018-01-11 01:57:43 +0100

    Added order saving to firebase from checkout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,7 @@ import { ProductFilterComponent } from './products/product-filter/product-filter
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ShoppingCartService } from './shopping-cart.service';
 import { ProductQuantityComponent } from './product-quantity/product-quantity.component';
+import { OrderService } from './order.service';
 
 
 @NgModule({
@@ -92,7 +93,8 @@ import { ProductQuantityComponent } from './product-quantity/product-quantity.co
     AdminAuthGuard,
     CategoryService,
     ProductService,
-    ShoppingCartService
+    ShoppingCartService,
+    OrderService
   ],
   bootstrap: [AppComponent]
 })
